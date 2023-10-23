67aedd355715bb8062110a7d8940f90faa2a52f9
RaviHelon-msf <ravihelon@gmail.com>
2023-10-23 16:35:37 -0300

    Feat:Add Ruby Caesar Cypher
    My first ruby code. It's a piece of garbo that makes a caesar cypher version of a text

---
+++ Ruby/caesar_cypher.rb
@@ -0,0 +1,7 @@
+def caesar_cypher(plain_text='aAZz', shift=1)
+    plain_text.chars.map do |char|
+      char == char.downcase ? ((char.ord - 97 + shift)%26+97).chr : ((char.ord - 65 + shift)%26+65).chr
+    end.join
+end
+
+puts caesar_cypher('baBuinoz',10)
